7931759409cacd30cfe6ef989705040b14e375b4
Lewis Campbell <5160938+lewismcampbell@users.noreply.github.com>
2020-12-07 16:06:52 +0000

    Doing the Header of the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,34 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* Header/About */}
+    <section className="container">
+      <div className="item about">
+        <div className="content">
+          <h1>Lewis Matthew Campbell</h1>
+          <p>This is jsut to check how the paragraphs are working.</p>
+          <p>This is jsut to check how the paragraphs are working.</p>
+          <p>This is jsut to check how the paragraphs are working.</p>
+          <p>This is jsut to check how the paragraphs are working.</p>
+        </div>
+      </div>
+      <div className="item headerImage">
+        <img src="https://i.imgur.com/2ph07zo.png" />
+      </div>
+    </section>
+    {/* Blog */}
+    <section>
+
+    </section>
+    {/* Contact */}
+    <section>
+
+    </section>
   </Layout>
 )
 
