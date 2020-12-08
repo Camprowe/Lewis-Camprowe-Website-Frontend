cecba450a13c11e4c2a9cf444291ac7e9de2a6bb
Lewis Campbell <5160938+lewismcampbell@users.noreply.github.com>
2020-12-08 11:05:32 +0000

    Starting Again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,5 @@
 import React from "react"
-import { Link, graphql} from "gatsby"
-import Img from "gatsby-image"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const IndexPage = ({data}) => (
-  <Layout>
-    <SEO title="Home" />
-    {/* Header/About */}
-    <section className="container">
-      <div className="item about">
-        <div className="content">
-          <h1>Lewis Matthew Campbell</h1>
-          <p>This is jsut to check how the paragraphs are working.</p>
-          <p>This is jsut to check how the paragraphs are working.</p>
-          <p>This is jsut to check how the paragraphs are working.</p>
-          <p>This is jsut to check how the paragraphs are working.</p>
-        </div>
-      </div>
-      <div className="item headerImage">
-        <Img fixed={data.file.childImageSharp.fixed} alt="THis is an image of Lewis Matthew Campbell"/>
-      </div>
-    </section>
-    {/* Blog */}
-    <section>
-
-    </section>
-    {/* Contact */}
-    <section>
-
-    </section>
-  </Layout>
-)
-
-export default IndexPage
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "Profile_Picture.png" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fixed(quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`;
+export default function Home() {
+  return <div>Hello world!</div>
+}
