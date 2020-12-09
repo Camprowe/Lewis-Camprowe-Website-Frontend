950f6d319161623ba0254cb15cdbf862039d98fe
Lewis Campbell <5160938+lewismcampbell@users.noreply.github.com>
2020-12-09 12:21:03 +0000

    Fixed the Image

--- src/components/homeHeader/homeHeader.js
+++ src/components/homeHeader/homeHeader.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../header/header"
 import homeHeaderStyles from "./homeHeader.module.css"
+import Img from "gatsby-image"
 
-export default function homeHeader() {
+export default function homeHeader(props) {
   return <section className={homeHeaderStyles.homeHeader}>
         <Header />
-        <img src="https://images.unsplash.com/photo-1602525655920-664869d87021?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" className={homeHeaderStyles.headerImage} alt="..." />
+        <Img fluid={props.headerImage} className={homeHeaderStyles.headerImage} alt="..." />
+        {/* <img src="https://images.unsplash.com/photo-1602525655920-664869d87021?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" className={homeHeaderStyles.headerImage} alt="..." /> */}
           
           <div className={homeHeaderStyles.make}>
             <h2>Make</h2>
@@ -13,4 +15,6 @@ export default function homeHeader() {
           </div>
         </section>
 }
+
+
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import React from "react"
 import HomeHeader from "../components/homeHeader/homeHeader"
+import { graphql } from "gatsby"
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <>
-      <HomeHeader />
+      <HomeHeader headerImage={data.file.childImageSharp.fixed} />
       <section className="homeAbout">
         <h2>About</h2>
       </section>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "Venice.png" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(quality: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
