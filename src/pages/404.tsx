2cdfa6540298c87dd38755951cc2249e6258c94b
Lewis Campbell <github@lewismcampbell.com>
2023-08-25 12:43:19 +0100

    Update to be single large page with text in the center

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,45 +1,21 @@
 import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import { HeadFC, PageProps } from "gatsby"
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="bg-secondary h-screen">
+      {/* Body */}
+      <section className="flex items-center h-5/6 justify-center">
+        <div className="mx-auto max-w-[43rem]">
+          <div className="text-center">
+            <p className="text-lg font-extralight leading-8 text-dark">404 Page not Found</p>
+            <h1 className="mt-3 text-[3.5rem] font-semibold leading-[4rem] tracking-tight text-black"></h1>
+          </div>
+          <div className="mt-6 flex items-center justify-center gap-4">
+          <a href="/" className="transform rounded-md border border-primary px-5 py-3 font-regular text-primary"> Return Home </a>
+        </div>
+        </div>
+      </section>
     </main>
   )
 }
