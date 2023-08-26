30c6b86114ebf8fb94e02192648edf31709346c5
Lewis Campbell <github@lewismcampbell.com>
2023-08-26 22:38:33 +0100

    Update 404 with the last words of Uriel Septim

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,11 +8,11 @@ const NotFoundPage: React.FC<PageProps> = () => {
       <section className="flex items-center h-5/6 justify-center">
         <div className="mx-auto max-w-[43rem]">
           <div className="text-center">
-            <p className="text-lg font-extralight leading-8 text-dark">404 Page not Found</p>
-            <h1 className="mt-3 text-[3.5rem] font-semibold leading-[4rem] tracking-tight text-black"></h1>
+            <p className="text-lg font-medium leading-8 text-dark">404 Page not Found</p>
+            <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">I can go no further. You alone must stand against the Prince of Destruction and his mortal servants. He must not have the Amulet of Kings! Take the Amulet. Give it to Jauffre. He alone knows where to find my last son. Find him, and close shut the jaws of Oblivion.</h1>
           </div>
           <div className="mt-6 flex items-center justify-center gap-4">
-          <a href="/" className="transform rounded-md border border-primary px-5 py-3 font-regular text-primary"> Return Home </a>
+          <a href="/" className="transform rounded-md border border-primary px-5 py-3 font-medium text-primary"> Return Home </a>
         </div>
         </div>
       </section>
